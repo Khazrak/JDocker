323d96e2f9c55f80f41019dd901bb518186a1cc0
Karl Andersson <karl.andersson1987@gmail.com>
2016-10-01 20:12:44 +0200

    Added a test for DockerImageName due to testing if it supporter registry/imagename (without repository)...which it didn't, so now fixed it

--- src/main/java/se/codeslasher/docker/utils/DockerImageName.java
+++ src/main/java/se/codeslasher/docker/utils/DockerImageName.java
@@ -11,37 +11,41 @@ import java.util.Optional;
 
 public class DockerImageName {
 
+    public static final String regexp = "[a-zA-Z0-9][a-zA-Z0-9_.-]";
+
     public DockerImageName(String image) {
-        String [] parts = image.split("/");
-        if(parts.length == 3) {
+        String[] parts = image.split("/");
+        if (parts.length == 3) {
             registry = Optional.of(new DockerRegistryName(parts[0]));
             repository = Optional.of(new DockerRepositoryName(parts[1]));
             imageName = new DockerImagePartName(parts[2]);
-        }
-        else if(parts.length == 2) {
-            repository = Optional.of(new DockerRepositoryName(parts[0]));
+        } else if (parts.length == 2) {
+            if (isRegistry(parts[0])) {
+                registry = Optional.of(new DockerRegistryName(parts[0]));
+            } else {
+                repository = Optional.of(new DockerRepositoryName(parts[0]));
+            }
             imageName = new DockerImagePartName(parts[1]);
-        }
-        else if(parts.length == 1) {
+        } else if (parts.length == 1) {
             imageName = new DockerImagePartName(parts[0]);
-        }
-        else {
-            throw new DockerClientException("Docker ImageInfo Name creation failed, "+image+" is not a valid image-name");
+        } else {
+            throw new DockerClientException("Docker ImageInfo Name creation failed, " + image + " is not a valid image-name");
         }
 
 
     }
+
 
     private Optional<DockerRegistryName> registry = Optional.empty();
     private Optional<DockerRepositoryName> repository = Optional.empty();
     private DockerImagePartName imageName;
 
     public String getImageRegistry() {
-        return this.registry.isPresent() ? this.registry.toString() : null;
+        return this.registry.isPresent() ? this.registry.get().registryName : null;
     }
 
     public String getImageRepo() {
-        return this.repository.isPresent() ? this.repository.toString() : null;
+        return this.repository.isPresent() ? this.repository.get().repoName : null;
     }
 
     public String getImageName() {
@@ -57,17 +61,18 @@ public class DockerImageName {
         private String repoName;
 
         public DockerRepositoryName(String part) {
-            if(part.contains(":") || part.contains("/")) {
+            if (part.contains(":") || part.contains("/")) {
                 throw new DockerClientException("Repository-name: " + part + " is not valid!");
             }
             repoName = part;
         }
     }
+
     public static class DockerRegistryName {
         private String registryName;
 
         public DockerRegistryName(String part) {
-            if(part.contains("/")) {
+            if (part.contains("/")) {
                 throw new DockerClientException("Repository-name: " + part + " is not valid!");
             }
             registryName = part;
@@ -79,15 +84,15 @@ public class DockerImageName {
         private String name;
 
         public DockerImagePartName(String name) {
-            String [] parts = name.split(":");
-            if(parts.length == 2) {
+            String[] parts = name.split(":");
+            if (parts.length == 2) {
                 this.name = parts[0];
-                this.version = parts [1];
-            }
-            else if(parts.length == 1) {
+                this.version = parts[1];
+            } else if (parts.length == 1) {
                 this.name = name;
             }
         }
+
         public String toString() {
             return name + ":" + version;
         }
@@ -98,8 +103,14 @@ public class DockerImageName {
 
         StringBuilder sb = new StringBuilder();
 
-        this.registry.ifPresent(drn -> {sb.append(drn.registryName);sb.append("/");});
-        this.repository.ifPresent(drn -> {sb.append(drn.repoName);sb.append("/");});
+        this.registry.ifPresent(drn -> {
+            sb.append(drn.registryName);
+            sb.append("/");
+        });
+        this.repository.ifPresent(drn -> {
+            sb.append(drn.repoName);
+            sb.append("/");
+        });
         sb.append(imageName.toString());
 
         return sb.toString();
@@ -107,22 +118,28 @@ public class DockerImageName {
 
     public String toStringWithoutTag() {
         StringBuilder sb = new StringBuilder();
-        this.registry.ifPresent(drn -> {sb.append(drn.registryName);sb.append("/");});
-        this.repository.ifPresent(drn -> {sb.append(drn.repoName);sb.append("/");});
+        this.registry.ifPresent(drn -> {
+            sb.append(drn.registryName);
+            sb.append("/");
+        });
+        this.repository.ifPresent(drn -> {
+            sb.append(drn.repoName);
+            sb.append("/");
+        });
         sb.append(imageName.name);
 
         return sb.toString();
     }
 
     public boolean equals(Object other) {
-        if(other == null) {
+        if (other == null) {
             return false;
         }
-        if(other instanceof DockerImageName == false) {
+        if (other instanceof DockerImageName == false) {
             return false;
         }
 
-        DockerImageName obj = (DockerImageName)other;
+        DockerImageName obj = (DockerImageName) other;
 
         return this.toString().equals(obj.toString());
     }
@@ -131,4 +148,7 @@ public class DockerImageName {
         return this.toString().hashCode();
     }
 
+    private boolean isRegistry(String part) {
+        return part.contains(":") || part.contains(".");
+    }
 }
