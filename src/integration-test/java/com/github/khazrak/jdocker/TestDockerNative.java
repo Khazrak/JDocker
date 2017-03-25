461cbca535741af005e85e2e0aa3f8237a8659c9
Karl Andersson <karl.andersson1987@gmail.com>
2017-03-25 23:03:14 +0100

    Added examples in TestDockerNative

--- src/integration-test/java/com/github/khazrak/jdocker/TestDockerNative.java
+++ src/integration-test/java/com/github/khazrak/jdocker/TestDockerNative.java
@@ -1,7 +1,11 @@
 package com.github.khazrak.jdocker;
 
+import com.github.khazrak.jdocker.abstraction.Container;
 import com.github.khazrak.jdocker.abstraction.DockerClient;
 
+import com.github.khazrak.jdocker.api126.DefaultDockerClient126;
+import com.github.khazrak.jdocker.api126.easy.EasyContainer;
+import com.github.khazrak.jdocker.api126.requests.ContainerCreationRequest126;
 import okhttp3.*;
 import org.junit.After;
 import org.junit.Before;
@@ -22,12 +26,27 @@ public class TestDockerNative {
     private static Logger logger = LoggerFactory.getLogger(TestDockerNative.class);
     public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 
-
-
-
-
     @org.junit.Test
     public void unix() throws IOException {
+        DockerClient client = new DefaultDockerClient126();
+
+        List<Container> containers = client.listContainers();
+
+        for(Container c : containers) {
+            System.out.println(c);
+        }
+
+        EasyContainer easyContainer = new EasyContainer("mongo");
+
+        easyContainer.addPublishPort("0.0.0.0", 1337, 27017);
+
+        easyContainer.name("my_easy_mongo");
+
+        ContainerCreationRequest126 creationRequest = easyContainer.buildRequest();
+
+        String container = client.createContainer(creationRequest);
+
+        System.out.println(container);
 
     }
 
